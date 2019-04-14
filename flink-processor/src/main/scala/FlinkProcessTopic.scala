8437f1ca56f354bb5e62b60bfdb116ab835d7f8a
Max Rockatansky <denisigoshev@gmail.com>
2019-04-14 17:52:41 +0300

    apache flink market data processing + cassandra persistence

--- flink-processor/src/main/scala/FlinkProcessTopic.scala
+++ flink-processor/src/main/scala/FlinkProcessTopic.scala
@@ -1,15 +1,20 @@
-import java.util.Properties
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.{Properties, UUID}
 
 import com.typesafe.scalalogging.Logger
-import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
-import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.time.Time
-import pureconfig.loadConfigOrThrow
-import pureconfig.generic.auto._
 import io.circe.parser.decode
-//import org.apache.flink.streaming.connectors.cassandra.CassandraSink
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import pureconfig.loadConfigOrThrow
+import org.apache.flink.streaming.api.datastream._
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor
+import org.apache.flink.streaming.connectors.cassandra.CassandraSink
+import pureconfig.generic.auto._
+
 
 /**
   * Reads from kafka topic and processes it using flink in windows
@@ -18,6 +23,7 @@ object FlinkProcessTopic extends App {
   // Some general config to get the actor system up and running
   val className = this.getClass.getName.split("\\$").last
   val streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+  streamExecutionEnvironment.setParallelism(1)
   implicit lazy val config: FlinkConfig = loadConfigOrThrow[FlinkConfig]
   implicit val logger = Logger(className)
 
@@ -27,20 +33,76 @@ object FlinkProcessTopic extends App {
   properties.setProperty("bootstrap.servers", config.kafkaConfig.port)
   properties.setProperty("group.id", config.kafkaConfig.groupId)
 
+
   val stream = streamExecutionEnvironment
     .addSource(
-      new FlinkKafkaConsumer011[String]("FXUS", new SimpleStringSchema(), properties)
+      new FlinkKafkaConsumer011[String]("FXUS", new SimpleStringSchema(),
+        properties)
         .setStartFromEarliest()
     )
-    .map(str => decode[Ticker](str).right.get)
-    .timeWindowAll(Time.minutes(1L))
-    .reduce((t1, t2) => Ticker(t1.tickerType, t1.time, t1.last, t1.volume + t2.volume)) // TODO: change for bar
-    .print // first log will appear in 1 minute
 
-//  CassandraSink.addSink(stream)
-//    .setQuery("INSERT INTO dev.emp (ticker) VALUES (?)")
-//    .setHost("127.0.0.1")
-//    .build()
+  final case class TickerCassandra(id: UUID,
+                                   ticker: String,
+                                   time: String,
+                                   price: String,
+                                   volume: Int,
+                                   currency: String
+                                  ){
+    override def toString: _root_.java.lang.String = s"$ticker $time $price $volume $currency"
+  }
+  val tickerStream: DataStream[Ticker] = stream.map { str =>
+    val tick = decode[Ticker](str).right.get
+    tick
+    }.assignTimestampsAndWatermarks(new AscendingTimestampExtractor[Ticker]() {
+      val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      override def extractAscendingTimestamp(element: Ticker): Long = {
+        val ts = new Timestamp(sdf.parse(element.time).getTime)
+        //println(ts.getTime)
+        ts.getTime
+      }
+    })
+  tickerStream.print()
+  val cassandraTickersStream = tickerStream.map{ tick =>
+    TickerCassandra(
+      UUID.randomUUID(),
+      tick.tickerType.toString,
+      tick.time,
+      tick.last.value.toString,
+      tick.volume,
+      tick.last.currency.toString
+    )
+  }
+  val bar1MinStream: DataStream[OHLC1Min] = tickerStream
+    .timeWindowAll(Time.minutes(1)).process(FlinkWindowProcessFunc)
+  bar1MinStream.print()
+
+   CassandraSink.addSink(bar1MinStream)
+     .setHost("localhost", 9042)
+     .setQuery("INSERT INTO " +
+       "test_keyspace.table_ohlc1m(id, ticker, timeStart, open, high, low, close , volume, " +
+       "currency) " +
+       "values (?, ?, ?, ?, ?, ?, ?, ?, ? );").build()
+
+   CassandraSink.addSink(cassandraTickersStream)
+   .setHost("localhost", 9042)
+   .setQuery("INSERT INTO test_keyspace" +
+     ".table_ticker(id, ticker, time, price, volume, currency )" +
+     "values (?, ?, ?, ?, ?, ?);").build()
 
   streamExecutionEnvironment.execute(config.jobName)
 }
+
+// start up optional steps ( win )  :
+// cd C:\zookeeper-3.4.12
+// zkServer.cmd
+// cd C:\kafka_2.12-2.1.0
+// .\bin\windows\kafka-server-start.bat .\config\server.properties
+// cassandra -f
+
+// cassandra prep STEPS:
+// docker exec -it cassandra cqlsh -e "CREATE KEYSPACE IF NOT EXISTS test_keyspace WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+// docker exec -it cassandra cqlsh -e "CREATE TABLE IF NOT EXISTS test_keyspace.table_ticker (id uuid, ticker text, time text,
+// price text, volume int, currency text, PRIMARY KEY (id));"
+
+// docker exec -it cassandra cqlsh -e "CREATE TABLE IF NOT EXISTS test_keyspace.table_ohlc1m (id uuid, ticker text timeStart text, open text, high text, low text, close text, volume int, currency text, PRIMARY KEY(id));"
+// docker exec -ot cassandra cqlsh -e "DROP TABLE test_keyspace.table_ticker;"
---
+++ flink-processor/src/main/scala/FlinkWindowProcessFunc.scala
@@ -0,0 +1,40 @@
+import java.util.UUID
+
+import org.apache.flink.streaming.api.scala.function._
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+
+object FlinkWindowProcessFunc extends ProcessAllWindowFunction[Ticker, OHLC1Min, TimeWindow] {
+  override def process(context: FlinkWindowProcessFunc.Context,
+                       xs: Iterable[Ticker], out: Collector[OHLC1Min]): Unit = {
+    val wtStart = xs.map(x => x.time).head
+    // val wtEnd = xs.map(x => x.time).last
+    val open = xs.map(x => x.last.value).head
+    val high = xs.map(x => x.last.value).max
+    val low = xs.map(x => x.last.value).min
+    val close = xs.map(x => x.last.value).last
+    val aggVolume = xs.map(x => x.volume).sum
+    out.collect(OHLC1Min(UUID.randomUUID(),
+      xs.head.tickerType.toString,
+      wtStart,
+      open.toString,
+      high.toString,
+      low.toString,
+      close.toString,
+      aggVolume,
+      xs.head.last.currency.toString))
+
+  }
+}
+case class OHLC1Min(id:UUID,
+                    ticker : String,
+                    tStart: String,
+                    open: String,
+                    high: String,
+                    low: String,
+                    close: String,
+                    vol: Int,
+                    currency: String ){
+  override def toString: String = s"OHLC1m[$ticker , $tStart, $open, $high $low $close $vol]"
+}
