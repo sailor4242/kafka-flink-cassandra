6b1b88f8f7e58c817e391cae5823c8c3c84310eb
Max Rockatansky <denisigoshev@gmail.com>
2019-04-14 22:33:10 +0300

    trashy commit managements

--- flink-processor/src/main/scala/FlinkModel.scala
+++ flink-processor/src/main/scala/FlinkModel.scala
@@ -11,6 +11,8 @@ import java.util.UUID
                       currency: String ){
     override def toString: String = s"OHLC1m[$ticker , $tStart, $open, $high $low $close $vol]"
   }
+
+
   final case class TickerCassandra(id: UUID,
                                    ticker: String,
                                    time: String,
